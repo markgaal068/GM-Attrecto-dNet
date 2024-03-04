9ee85463f075e13b252839d1ea6162d4475788c3
markgaal068 <gaalmark.markit@gmail.com>
2024-03-04 18:14:46 +0100

    Calc-Szorg1

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ CalculatorProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Calculator;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Calculator.Calculator c = new Calculator.Calculator();
+
+            Console.WriteLine("Írj be két számot a kivonáshoz:");
+            if (int.TryParse(Console.ReadLine(), out int a) && int.TryParse(Console.ReadLine(), out int b))
+            {
+                int result = c.Subtract(a, b);
+                Console.WriteLine($"Különbség: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Érvénytelen bemenet.");
+            }
+        }
+    }
+}
